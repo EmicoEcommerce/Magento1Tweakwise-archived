8e4b6daab0ee7572da6452b0a944bdc764f3f3e8
Bram Gerritsen <bgerritsen@emico.nl>
2018-09-06 11:29:36 +0200

    Cleaner and more robust solution than substring. Remove debug logging

--- app/code/community/Emico/Tweakwise/Helper/Seo.php
+++ app/code/community/Emico/Tweakwise/Helper/Seo.php
@@ -18,36 +18,19 @@ class Emico_Tweakwise_Helper_Seo extends Mage_Core_Helper_Abstract
     public function shouldApplyNoIndexNoFollow(Emico_Tweakwise_Model_Bus_Type_Facet $facetLinkedTo = null)
     {
         // Certain filter combinations are allowed for indexing
-        $this->debugLog('Check combination');
         if ($this->isInCombinationWhitelist($facetLinkedTo)) {
-            $this->debugLog('Combination is whitelisted');
             return false;
         }
 
-        $this->debugLog('Check number of filters');
         if ($this->exceedsAttributeLimit($facetLinkedTo)) {
-            $this->debugLog('Number of filters exceeded');
             return true;
         }
 
-        $this->debugLog('Check blacklist');
         if ($this->isInFacetBlacklist($facetLinkedTo)) {
-            $this->debugLog('In blacklist');
             return true;
         }
 
-        $this->debugLog('Apply index/follow');
         return false;
-    }
-
-    /**
-     * @param string $message
-     */
-    protected function debugLog(string $message)
-    {
-        if (isset($_GET['debug'])) {
-            Mage::log($message, Zend_Log::DEBUG, 'tweakwise_seo_debug.log');
-        }
     }
 
     /**
@@ -90,8 +73,9 @@ class Emico_Tweakwise_Helper_Seo extends Mage_Core_Helper_Abstract
 
         foreach ($selectedFacets as $facet) {
             $attributeCode = $facet->getFacetSettings()->getAttributeName();
+            $source = $facet->getFacetSettings()->getSource();
 
-            if (strpos($attributeCode, 'ae-') !== false) {
+            if ($source === Emico_Tweakwise_Model_Bus_Type_Facet_Settings::FACET_SOURCE_DERIVATIVE) {
                 return true;
             }
 
